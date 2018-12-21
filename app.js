394a13c8b12c399c74f3ce8694a5b5b28a6ef1d5
Wageesh95 <imwageesha@gmail.com>
2018-12-22 02:07:02 +0530

    ppp

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
-const PORT = 3000;
+//const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 const rtsIndex = require('./routes/index.router');
 
